923556f6887b518f8dc85246c8ceb4d7fdd4d4b1
Ruslana Melnik <rusalna0417@gmail.com>
2023-05-20 10:03:44 +0300

    Class for sending notifications using Firebase Messaging Service

---
+++ app/src/main/java/com/example/productiveteamapp/notification/FcmNotificationSender.java
@@ -0,0 +1,52 @@
+package com.example.productiveteamapp.notification;
+
+import android.util.Log;
+
+import org.json.JSONObject;
+
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class FcmNotificationSender {
+    public static void sendNotification(String topic, String title, String body) {
+        Thread thread = new Thread(() -> {
+            try {
+                String FCM_API_URL = "https://fcm.googleapis.com/fcm/send";
+                String SERVER_KEY = "AAAAOImkGIg:APA91bGP0wiQb-Rgt4r5tHd2_-dhjLKTIkOTSeQJWpTrw4rxRlGgTwUfUKWjI1jyS9UE2naahD8NdbdPTs1FYDfAdqabJfEod-08yJy5oxCAzrZ6aqCSxxwh_5kf6z217MScPdlevcFD";
+
+                URL url = new URL(FCM_API_URL);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setUseCaches(false);
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+                conn.setRequestMethod("POST");
+                conn.setRequestProperty("Authorization", "key=" + SERVER_KEY);
+                conn.setRequestProperty("Content-Type", "application/json");
+
+                JSONObject notification = new JSONObject();
+                notification.put("title", title);
+                notification.put("body", body);
+
+                JSONObject data = new JSONObject();
+                data.put("key1", "value1");
+                data.put("key2", "value2");
+
+                JSONObject payload = new JSONObject();
+                payload.put("notification", notification);
+                payload.put("data", data);
+                payload.put("to", "/topics/" + topic);
+
+                OutputStreamWriter writer = new OutputStreamWriter(conn.getOutputStream());
+                writer.write(payload.toString());
+                writer.flush();
+
+                int responseCode = conn.getResponseCode();
+                Log.d("PushNotificationSender", "Response Code: " + responseCode);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        thread.start();
+    }
+}
